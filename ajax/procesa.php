fa96c4684c60c4be604151a20392d42e8c664fab
ismael Ariel <ismael_ariel@hotmail.com>
2013-07-13 11:46:06 -0500

    actualizacion

--- ajax/procesa.php
+++ ajax/procesa.php
@@ -1,19 +1,24 @@
-<?php   
+<?php
 
-while( list( $field, $value ) = each( $_POST )) {
+//var_dump($_GET);   
+
+while( list( $field, $value ) = each( $_POST )) {	
    if( is_array( $value )) {
+   	  
       while( list( $arrayField, $arrayValue ) = each( $value )) {
-         echo "<p><strong>" .$field."</strong>:" . $arrayValue . "</p>\n";
+         echo "<p><strong>" .$arrayField."(POST)</strong>:" . $arrayValue . "</p>\n";
       }
    } else {
       echo "<p><strong>" .$field."</strong>:". $value . "</p>\n";
    }
 }
 
-while( list( $field, $value ) = each( $_GET )) {  
+
+while( list( $field, $value ) = each( $_GET )) {
+	echo $field.":".$value.", is array:".is_array( $value )."<br/>";  
    if( is_array( $value )) {      
       while( list( $arrayField, $arrayValue ) = each( $value )) {
-         echo "<p><strong>" .$field."</strong>:" . $arrayValue . "</p>\n";
+         echo "<p><strong>" .$arrayField."(GET)</strong>:" . $arrayValue . "</p>\n";
       }
    } else {
       echo "<p><strong>" .$field."</strong>:". $value . "</p>\n";
